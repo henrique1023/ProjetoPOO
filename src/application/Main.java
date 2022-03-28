9b2a6c0d90f84b6589a8501618f1906204d29622
henrique1023 <henrique.amorim@fatec.sp.gov.br>
2022-03-28 20:50:48 -0300

    Primeiro commit

---
+++ src/application/Main.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	private static Scene mainScene;
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			// essa instacia serve para manipular a tela mais facilmente
+			// adicionando o arquivo do FXML
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			ScrollPane scrollPane = loader.load();
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+
+			mainScene = new Scene(scrollPane);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Consulto Mario Cantano");
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
