1ecb84d47628076002c398d9f875c71dadf191fe
henrique1023 <henrique.amorim@fatec.sp.gov.br>
2022-04-01 13:55:13 -0300

    Tela Paciente e Form de cadastro

---
+++ src/gui/PacienteFormController.java
@@ -0,0 +1,170 @@
+package gui;
+
+import java.net.URL;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import model.entities.Paciente;
+import model.exceptions.ValidationException;
+import model.services.PacienteService;
+
+public class PacienteFormController implements Initializable {
+
+	private Paciente entidade;
+
+	private PacienteService service;
+
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	@FXML
+	private TextField txtId;
+
+	@FXML
+	private TextField txtNome;
+
+	@FXML
+	private DatePicker dpDataAniv;
+	
+	@FXML
+	private Label labelErrorNome;
+	
+	@FXML
+	private Label labelErrorData;
+
+	public void setEntidade(Paciente entidade) {
+		this.entidade = entidade;
+	}
+
+	public void setService(PacienteService service) {
+		this.service = service;
+	}
+
+	// esse metodo inscreve a outra interface para ouvir essa interface
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+
+	// esse metodo que gera o novo alerta para o sistema
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
+	}
+
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		if (entidade == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entidade = getFormData();
+			service.saveOrUptade(entidade);
+			// quando é confirmado o salvar ele emite um alerta para atualizar a pagina
+			notifyDataChangeListeners();
+			Utils.currentStage(event).close();
+		} catch (ValidationException e) {
+			setErrorsMessages(e.getErrors());
+		} catch (DbException e) {
+			Alerts.showAlert("Error savings Object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+	private Paciente getFormData() {
+		Paciente obj = new Paciente();
+
+		ValidationException exception = new ValidationException("Validation Errors");
+
+		obj.setIdPaciente(Utils.tryParseToInt(txtId.getText()));
+
+		obj.setNomePaciente(txtNome.getText());
+		
+		if(txtNome.getText() == null || txtNome.getText().trim().equals("")) {
+			exception.addError("nome", "Fields can't be emply");
+		}
+		
+		if (dpDataAniv.getValue() == null) {
+			exception.addError("dataAniv", "Fields can't be emply");
+		}else {
+			Instant instant = Instant.from(dpDataAniv.getValue().atStartOfDay(ZoneId.systemDefault()));
+		
+			obj.setDataAniversario(Date.from(instant));
+		}
+
+		if (exception.getErrors().size() > 0) {
+			throw exception;
+		}
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	public void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtNome, 30);
+		Utils.formatDatePicker(dpDataAniv, "dd/MM/yyyy");
+	}
+
+	// Esse metodo inicia os valores no campos
+	public void updateFormData() {
+		if (entidade == null) {
+			throw new IllegalStateException("Entity was null!!");
+		}
+		txtId.setText(String.valueOf(entidade.getIdPaciente()));
+		txtNome.setText(entidade.getNomePaciente());
+
+		// se a data na entidade estiver vazia ele manda nulo para a tela
+		if (entidade.getDataAniversario() != null) {
+			dpDataAniv.setValue(LocalDate.ofInstant(entidade.getDataAniversario().toInstant(), ZoneId.systemDefault()));
+		}
+	}
+	
+	//esse metodo verifica se tem o erro e manda ele para o label
+		public void setErrorsMessages(Map<String, String> errors) {
+			Set<String> fields = errors.keySet();
+			
+			if(fields.contains("nome")) {
+				labelErrorNome.setText(errors.get("nome"));
+			}
+			
+			if(fields.contains("dataAniv")) {
+				labelErrorNome.setText(errors.get("dataAniv"));
+			}
+		}
+
+}
--- src/gui/PacienteListController.java
+++ src/gui/PacienteListController.java
@@ -1,22 +1,34 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import application.Main;
+import db.DbException;
 import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
 import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Paciente;
 import model.services.PacienteService;
@@ -38,12 +50,22 @@ public class PacienteListController implements Initializable, DataChangeListener
 	private TableColumn<Paciente, Date> tableColumnData;
 	
 	@FXML
+	private TableColumn<Paciente, Paciente> tableColumnEDIT;
+
+	@FXML
+	private TableColumn<Paciente, Paciente> tableColumnREMOVE;
+	
+	
+	@FXML
 	private Button btNovo;
 	
 	private ObservableList<Paciente> obsList;
 	
 	@FXML
 	public void onBtNovoAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		Paciente obj = new Paciente();
+		createDialogForm(obj, "/gui/PacienteForm.fxml", parentStage);
 		
 	}
 	
@@ -63,6 +85,41 @@ public class PacienteListController implements Initializable, DataChangeListener
 		tableViewPaciente.prefHeightProperty().bind(stage.heightProperty());
 	}
 	
+	private void createDialogForm(Paciente obj, String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+
+			// esse metodo injeta o paciente iniciando o objeto
+			// sempre que tem que ter um objeto no formulario
+			// precisa injetar ele aqui
+			PacienteFormController controller = loader.getController();
+			controller.setEntidade(obj);
+			controller.setService(new PacienteService());
+			// esse metodo que gera a atualização após salvar um novo departamento
+			controller.subscribeDataChangeListener(this);
+			controller.updateFormData();
+
+			Stage dialogStage = new Stage();
+
+			dialogStage.setTitle("Enter Department Data");
+			// passa o FXML departmentForm como cena
+			dialogStage.setScene(new Scene(pane));
+			// Essa janela não pode ser redimencionada
+			dialogStage.setResizable(false);
+			// Informa qual é o stage pai dessa nova janela
+			dialogStage.initOwner(parentStage);
+			// informa que não pode mexer no programa sem fechar essa janela
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		} catch (IOException e) {
+			e.printStackTrace();
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	
+	
 	public void updateTableView() {
 		if (paciente == null) {
 			throw new IllegalStateException("Service was null");
@@ -72,8 +129,8 @@ public class PacienteListController implements Initializable, DataChangeListener
 		// somente um ObservableList pode passar parametros para o setItems
 		obsList = FXCollections.observableArrayList(list);
 		tableViewPaciente.setItems(obsList);
-//		initEditButtons();
-//		initRemoveButtons();
+		initEditButtons();
+		initRemoveButtons();
 	}
 	
 	
@@ -86,6 +143,62 @@ public class PacienteListController implements Initializable, DataChangeListener
 	@Override
 	public void onDataChanged() {
 		updateTableView();
+	}
+	
+	// ESSE METODO CRIA OS BOTÕES DE EDITAR NO PAINEL
+	private void initEditButtons() {
+		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEDIT.setCellFactory(param -> new TableCell<Paciente, Paciente>() {
+			private final Button button = new Button("Editar");
+
+			@Override
+			protected void updateItem(Paciente obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(
+						event -> createDialogForm(obj, "/gui/PacienteForm.fxml", Utils.currentStage(event)));
+			}
+		});
+	}
+
+	// ESSE METODO CRIA OS BOTÕES DE REMOVER NO PAINEL
+	private void initRemoveButtons() {
+		tableColumnREMOVE.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnREMOVE.setCellFactory(param -> new TableCell<Paciente, Paciente>() {
+			private final Button button = new Button("Delete");
+
+			@Override
+			protected void updateItem(Paciente obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(event -> removeEntity(obj));
+			}
+
+		});
+	}
+	
+	private void removeEntity(Paciente obj) {
+		Optional<ButtonType> result = Alerts.showConfirmation("Confirmation", "Are you sure to delete?");
+		
+		if(result.get() == ButtonType.OK) {
+			if(paciente == null) {
+				throw new IllegalStateException("Service was null");
+			}
+			try {
+				paciente.remove(obj);
+				updateTableView();
+			}catch (DbException e) {
+				Alerts.showAlert("Error removing object", null, e.getMessage(), AlertType.ERROR);
+			}
+		}
 	}
 
 }
